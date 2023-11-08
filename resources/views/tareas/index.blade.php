ddb96c92e55b3780ddfceaf4a2ea9235619dbb9f
Jovany Vergara <diego.jobany11@gmail.com>
2023-11-08 17:40:39 -0600

    [master] change design

--- resources/views/tareas/index.blade.php
+++ resources/views/tareas/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container w-25 border p-4">
+<div class="container w-25 border p-4 mt-4">
   <form>
     <div class="mb-3">
       <label for="title" class="form-label">Titulo de tarea</label>
