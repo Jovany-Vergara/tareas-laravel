b5ccdc7e4fd94eaa5a0f2e2dcbe7cb028f8634e8
Jovany Vergara <diego.jobany11@gmail.com>
2023-11-07 18:03:08 -0600

    [master] create new desing in of new template

---
+++ resources/views/tareas/index.blade.php
@@ -0,0 +1,15 @@
+@extends('app')
+
+@section('content')
+
+<div class="container w-25 border p-4">
+  <form>
+    <div class="mb-3">
+      <label for="title" class="form-label">Titulo de tarea</label>
+      <input type="title" class="form-control" id="title">
+    </div>
+    <button type="submit" class="btn btn-primary">Crear una tarea</button>
+  </form>
+</div>
+
+@endsection
