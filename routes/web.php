aba986d53b3d36089bdc59373c2221638ff18063
Jovany Vergara <diego.jobany11@gmail.com>
2023-11-07 18:02:04 -0600

    [master] create new routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('app');
+});
+
+Route::get('/home', function () {
     return view('welcome');
+});
+
+Route::get('/tareas', function () {
+    return view('tareas.index');
 });
